4c7e0a7f72a810bbe243e2315386925438978b0d
uhuikim <uhui94@naver.com>
2023-10-23 17:21:36 +0900

    feat : 프로젝트 세팅

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+    experimental: {
+        outputFileTracingIncludes: {
+            '/*': ['./content/**/*']
+        }
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+        prependData: `@import "@/styles/base/_variables.scss";` // prependData 옵션 추가
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'github.com',
+                port: '',
+                pathname: '/**'
+            }
+        ]
+    }
+};
